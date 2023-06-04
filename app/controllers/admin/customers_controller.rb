0fd149a10e0db925a09bbfda239674e67ba8f33f
yusuke0622 <132802034+yusuke0622@users.noreply.github.com>
2023-06-04 13:03:34 +0000

    Add admin_customer edit_page

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,5 +8,21 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+    flash[:success] = "You have updated user info successfully."
+    redirect_to admin_customer_path(@customer.id)
+    end
+  end
+  
+   private 
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana,
+                                        :postal_code, :address, :telephone_number, :email)
+  end
+  
 end
