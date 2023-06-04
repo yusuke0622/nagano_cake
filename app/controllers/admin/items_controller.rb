f23756d63ab20f39353db21195961cad7f6f64b4
yusuke0622 <132802034+yusuke0622@users.noreply.github.com>
2023-06-04 03:55:14 +0000

    Add admin_item show_page

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -14,6 +14,7 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  
+  def add_tax_price
+      (self.price * 1.10).round
+  end
 end
