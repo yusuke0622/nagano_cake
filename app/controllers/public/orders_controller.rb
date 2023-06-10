78c15d82a85aeea52997051bb38c3520181a4d26
yusuke0622 <132802034+yusuke0622@users.noreply.github.com>
2023-06-10 13:47:51 +0000

    order confirm_page

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,10 +3,26 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
     @customer = current_customer
   end
+  
+  def confirm
+    @order = Order.new(order_params)
+    @order.postal_code = current_customer.postal_code
+    @order.destination = current_customer.address
+    @order.address_name = current_customer.first_name + current_customer.last_name
+    @cart_items = current_customer.cart_items
+    @total_price = @cart_items.sum{|cart_item|cart_item.item.add_tax_price * cart_item.amount}
+    @order.postage = 800
+  end
 
   def index
   end
 
   def show
   end
+  
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :destination, :address_name)
+  end
+  
 end
