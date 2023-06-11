ba830b739c208192bda7a901e81f52b023fa5e5e
yusuke0622 <132802034+yusuke0622@users.noreply.github.com>
2023-06-11 09:24:16 +0000

    Add order index_page

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,14 +4,29 @@ class Public::OrdersController < ApplicationController
     @customer = current_customer
   end
   
+  def index
+    @orders = current_customer.orders.all
+  end
+  
   def confirm
     @order = Order.new(order_params)
-    @order.postal_code = current_customer.postal_code
-    @order.destination = current_customer.address
-    @order.address_name = current_customer.first_name + current_customer.last_name
     @cart_items = current_customer.cart_items
+    @order.customer_id = current_customer.id
     @total_price = @cart_items.sum{|cart_item|cart_item.item.add_tax_price * cart_item.amount}
     @order.postage = 800
+    
+    if params[:order][:destination_number] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.destination = current_customer.address
+      @order.address_name = current_customer.first_name + current_customer.last_name
+      render :confirm
+    elsif params[:order][:destination_number] == "1"
+      @order.postal_code = params[:order][:postal_code]
+      @order.destination = params[:order][:destination]
+      @order.address_name = params[:order][:address_name]
+    else
+      render :new
+    end
   end
   
   def create
@@ -20,11 +35,11 @@ class Public::OrdersController < ApplicationController
     if @order.save!
       @cart_items = current_customer.cart_items
       @cart_items.each do |cart_item|
-        order_detail = OrderDetail.new
-        order_detail.item_id = cart_item.item.id
-        order_detail.order_id = @order.id
+        order_detail = OrderDetail.new(order_id: @order.id)
+        order_detail.item_id = cart_item.item_id
         order_detail.amount = cart_item.amount
         order_detail.price = cart_item.item.price
+        order_detail.save
       end
       redirect_to orders_thanx_path
       @cart_items.destroy_all
@@ -35,10 +50,8 @@ class Public::OrdersController < ApplicationController
   
   def thanx
   end
-  
 
-  def index
-  end
+  
 
   def show
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
   has_one_attached :image
   
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy
+  has_many :order_detail, dependent: :destroy
   
   def get_image(width, height)
     unless image.attached?
