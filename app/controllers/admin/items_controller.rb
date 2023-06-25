5ec9ada9c30ae32a2d13f2cae9b19dc675625e3d
yusuke0622 <132802034+yusuke0622@users.noreply.github.com>
2023-06-25 10:24:41 +0000

    テスト前調整

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -12,7 +12,7 @@ class Admin::ItemsController < ApplicationController
   end
   
   def index
-    @items = Item.page(params[:page])
+    @items = Item.page(params[:page]).per(10)
   end
 
   def show
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,7 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page])
+    @items = Item.page(params[:page]).order('id DESC')
+    @items_all = Item.all
   end
 
   def show
