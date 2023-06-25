b90c791899b46f6fce391862bc2fb53eabcf3d65
yusuke0622 <132802034+yusuke0622@users.noreply.github.com>
2023-06-25 01:18:13 +0000

    Update layout6

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -59,4 +59,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def after_sign_up_path_for(resources)
+     my_page_path(current_customer.id)
+  end
 end
